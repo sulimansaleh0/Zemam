040810820c3e125b784780f5d0a5120a74bcd8ba
kamelmaher <kamelmahersh@gmail.com>
2026-08-12 15:02:17 +0300

    validators

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,16 @@
-const { login, signup, logout } = require("../controllers/user.controller")
-
 const router = require("express").Router()
 
-router.post("/login", login)
-router.post("/signup", signup)
+// Middlewares
+const validate = require("../middlewares/validator")
+
+// Controllers
+const { login, signup, logout } = require("../controllers/user.controller")
+
+// Schemas
+const { loginSchema, signupSchema } = require("../validators/user")
+
+router.post("/login", loginSchema, validate, login)
+router.post("/signup", signupSchema, validate, signup)
 router.post("/logout", logout)
 
 module.exports = router
